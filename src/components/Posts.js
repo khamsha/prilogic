de0ee1cd605c95795ae211a4316ec5115880c0d8
khamsha <42925247+khamsha@users.noreply.github.com>
2018-11-14 17:11:11 +0300

    Prilogic first commit

---
+++ src/components/Posts.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+
+import { Button } from 'reactstrap';
+import { Table } from 'reactstrap'
+import StarRatingComponent from 'react-star-rating-component'
+
+const Posts = ({posts}) => (
+  <Table>
+    <thead>
+          <tr>
+            <th>#</th>
+            <th>Store id</th>
+            <th>Qty</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+    <tbody>
+    {posts.slice(1).map((post, i) =>
+      <tr key={i}>
+      <th scope="row">{post.transaction}</th>
+      <th>{post.storeId}</th>
+      <th>{post.qty}</th>
+      <th><Button key={i}>Edit</Button></th>
+      <th><StarRatingComponent 
+          name={i} 
+          starCount={5}
+          starColor={"blue"}
+        /></th>
+      </tr>
+    )}
+    </tbody>
+  </Table>
+)
+
+Posts.propTypes = {
+  posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+});
+
+export default connect(mapStateToProps)(Posts)
---
+++ src/credentials.js
@@ -0,0 +1,8 @@
+export const clientId = "15529117821-o28i477kvrr8tauhld51mc1pij1ju8tv.apps.googleusercontent.com";
+export const scope = "https://www.googleapis.com/auth/spreadsheets";
+export const spreadsheetId = "1_Lx2XatJWqSXjHvBf-VMj02ycFz3KnDh28rEbVU537I";
+export const apiKey = "AIzaSyBxH4AqxD3XeUqK8h9EU6Ju0KxFe8n5Y1s";
+export const discoveryDocs = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+export const range = "C:E";
+
+  //clientSecret: "tkG3kVHsIVilNEPASbiXyv3W"
